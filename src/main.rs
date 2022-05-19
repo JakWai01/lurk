a1b20f8658a6fe65f425bddd49acde56553885f3
JakWai01 <jakob.waibel@web.de>
2022-05-19 19:50:26 +0200

    feat: Add poc to trace syscalls

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
+mod system_call_names;
+
+use linux_personality::personality;
 use nix::sys::ptrace;
+use nix::sys::wait::wait;
 use nix::unistd::{fork, ForkResult, Pid};
+use std::os::unix::process::CommandExt;
+use std::process::{exit, Command};
 
 fn main() {
-    match unsafe{fork()} {
+    match unsafe { fork() } {
         Ok(ForkResult::Child) => {
-            run_child();
+            run_tracee();
         }
 
-        Ok(ForkResult::Parent {child}) => {
-            run_parent(child);
+        Ok(ForkResult::Parent { child }) => {
+            run_tracer(child);
         }
 
         Err(err) => {
             panic!("[main] fork() failed: {}", err);
         }
     }
-
-    loop{}
 }
 
-fn run_parent(_child: Pid) {}
+fn run_tracer(child: Pid) {
+    wait().unwrap();
 
-fn run_child() {
+    match ptrace::getregs(child) {
+        Ok(x) => println!(
+            "Syscall number: {:?}",
+            system_call_names::SYSTEM_CALL_NAMES[(x.orig_rax) as usize]
+        ),
+        Err(x) => println!("{:?}", x),
+    };
+}
+
+fn run_tracee() {
     ptrace::traceme().unwrap();
+    personality(linux_personality::ADDR_NO_RANDOMIZE).unwrap();
+
+    Command::new("ls").exec();
+
+    exit(0)
 }
