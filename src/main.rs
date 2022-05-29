71fcb7d142f4e1ccec543c13961bb9500ae076e9
JakWai01 <jakob.waibel@web.de>
2022-05-29 21:55:29 +0200

    fix: Formatting

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 mod system_call_names;
 
+use byteorder::{LittleEndian, WriteBytesExt};
+use libc::{c_long, c_void};
 use linux_personality::personality;
 use nix::sys::ptrace;
+use nix::sys::ptrace::AddressType;
 use nix::sys::wait::wait;
 use nix::unistd::{fork, ForkResult, Pid};
 use std::os::unix::process::CommandExt;
 use std::process::{exit, Command};
-use libc::{c_long, c_void};
-use nix::sys::ptrace::AddressType;
-use byteorder::{LittleEndian, WriteBytesExt};
-
 
 fn main() {
     match unsafe { fork() } {
@@ -27,8 +26,6 @@ fn main() {
     }
 }
 
-// The second rax contains the return value
-// If this is larger than 10000, we can assume it is an address and has to be converted to hex
 fn run_tracer(child: Pid) {
     loop {
         wait().unwrap();
@@ -37,17 +34,31 @@ fn run_tracer(child: Pid) {
 
         match ptrace::getregs(child) {
             Ok(x) => {
-                
                 if x.orig_rax == 0 || x.orig_rax == 1 || x.orig_rax == 2 {
                     reg = x.rsi;
 
                     let syscall_tuple = system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize];
 
-                    let argument_type_array: [system_call_names::SystemCallArgumentType; 6] = [syscall_tuple.1, syscall_tuple.2, syscall_tuple.3, syscall_tuple.4, syscall_tuple.5, syscall_tuple.6];
-                    
-                    println!("{:?}", system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0);
+                    let argument_type_array: [system_call_names::SystemCallArgumentType; 6] = [
+                        syscall_tuple.1,
+                        syscall_tuple.2,
+                        syscall_tuple.3,
+                        syscall_tuple.4,
+                        syscall_tuple.5,
+                        syscall_tuple.6,
+                    ];
 
-                    let mut output = format!("[{:?}] {}(", child.as_raw(), system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0);
+                    println!(
+                        "{:?}",
+                        system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0
+                    );
+
+                    let mut output = format!(
+                        "[{:?}] {}(",
+                        child.as_raw(),
+                        system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0
+                    );
+                    let mut first_comma = true;
 
                     for (i, arg) in argument_type_array.iter().enumerate() {
                         let value = match i {
@@ -57,23 +68,38 @@ fn run_tracer(child: Pid) {
                             3 => x.r10,
                             4 => x.r8,
                             5 => x.r9,
-                            _ => panic!("Invalid system call definition!")
+                            _ => panic!("Invalid system call definition!"),
                         };
 
+                        match arg {
+                            system_call_names::SystemCallArgumentType::None => continue,
+                            system_call_names::SystemCallArgumentType::Integer
+                            | system_call_names::SystemCallArgumentType::String
+                            | system_call_names::SystemCallArgumentType::Address => {
+                                if first_comma {
+                                    first_comma = false;
+                                } else {
+                                    output.push_str(", ")
+                                }
+                            }
+                        }
 
                         match arg {
                             system_call_names::SystemCallArgumentType::Integer => {
-                                output.push_str(format!("{:?}, ", value).as_str());
-                            },
+                                output.push_str(format!("{:?}", value).as_str());
+                            }
                             system_call_names::SystemCallArgumentType::String => {
-                                output.push_str(format!("{:?}, ", read_string(child, reg as *mut c_void)).as_str());
-                            },
+                                output.push_str(
+                                    format!("{:?}", read_string(child, reg as *mut c_void))
+                                        .as_str(),
+                                );
+                            }
                             system_call_names::SystemCallArgumentType::Address => {
-                                output.push_str(format!("{:?}, ", value).as_str());
-                            },
+                                output.push_str(format!("{:?}", value).as_str());
+                            }
                             system_call_names::SystemCallArgumentType::None => {
                                 continue;
-                            },
+                            }
                         }
                     }
 
@@ -88,9 +114,9 @@ fn run_tracer(child: Pid) {
                             reg = x.rsi;
                             x
                         },
-                    )}
+                    )
                 }
-            ,
+            }
             Err(_) => break,
         };
 
@@ -125,20 +151,11 @@ fn read_string(pid: Pid, address: AddressType) -> String {
         let mut bytes: Vec<u8> = vec![];
         let address = unsafe { address.offset(count) };
 
-        println!("{:?}", address);
-        
-        // let res: c_long = ptrace::read(pid, address).unwrap_or_else(|err| {
-        //     // panic!("Failed to read data for pid {}: {}", pid, err);
-        //     continue;
-        // });
-
         let res: c_long;
 
         match ptrace::read(pid, address) {
-            Ok(c_long) => {
-                res = c_long
-            },
-            Err(_) => break 'done
+            Ok(c_long) => res = c_long,
+            Err(_) => break 'done,
         }
 
         bytes.write_i64::<LittleEndian>(res).unwrap_or_else(|err| {
