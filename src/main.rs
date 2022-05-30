0e3227e0d5f8fd19d888731f9a92bed37ddfdcd7
JakWai01 <jakob.waibel@web.de>
2022-05-30 21:36:21 +0200

    feat: Integrate SYSTEM_CALLS for all syscalls

--- src/main.rs
+++ src/main.rs
@@ -34,101 +34,79 @@ fn run_tracer(child: Pid) {
 
         match ptrace::getregs(child) {
             Ok(x) => {
-                if x.orig_rax == 0 || x.orig_rax == 1 || x.orig_rax == 2 {
-                    reg = x.rsi;
+                reg = x.rsi;
 
-                    let syscall_tuple = system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize];
+                let syscall_tuple = system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize];
 
-                    let argument_type_array: [system_call_names::SystemCallArgumentType; 6] = [
-                        syscall_tuple.1,
-                        syscall_tuple.2,
-                        syscall_tuple.3,
-                        syscall_tuple.4,
-                        syscall_tuple.5,
-                        syscall_tuple.6,
-                    ];
+                let argument_type_array: [system_call_names::SystemCallArgumentType; 6] = [
+                    syscall_tuple.1,
+                    syscall_tuple.2,
+                    syscall_tuple.3,
+                    syscall_tuple.4,
+                    syscall_tuple.5,
+                    syscall_tuple.6,
+                ];
 
-                    println!(
-                        "{:?}",
-                        system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0
-                    );
+                let mut output = format!(
+                    "[{:?}] {}(",
+                    child.as_raw(),
+                    system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0
+                );
+                let mut first_comma = true;
 
-                    let mut output = format!(
-                        "[{:?}] {}(",
-                        child.as_raw(),
-                        system_call_names::SYSTEM_CALLS[(x.orig_rax) as usize].0
-                    );
-                    let mut first_comma = true;
+                for (i, arg) in argument_type_array.iter().enumerate() {
+                    let value = match i {
+                        0 => x.rdi,
+                        1 => x.rsi,
+                        2 => x.rdx,
+                        3 => x.r10,
+                        4 => x.r8,
+                        5 => x.r9,
+                        _ => panic!("Invalid system call definition!"),
+                    };
 
-                    for (i, arg) in argument_type_array.iter().enumerate() {
-                        let value = match i {
-                            0 => x.rdi,
-                            1 => x.rsi,
-                            2 => x.rdx,
-                            3 => x.r10,
-                            4 => x.r8,
-                            5 => x.r9,
-                            _ => panic!("Invalid system call definition!"),
-                        };
-
-                        match arg {
-                            system_call_names::SystemCallArgumentType::None => continue,
-                            system_call_names::SystemCallArgumentType::Integer
-                            | system_call_names::SystemCallArgumentType::String
-                            | system_call_names::SystemCallArgumentType::Address => {
-                                if first_comma {
-                                    first_comma = false;
-                                } else {
-                                    output.push_str(", ")
-                                }
-                            }
-                        }
-
-                        match arg {
-                            system_call_names::SystemCallArgumentType::Integer => {
-                                output.push_str(format!("{:?}", value).as_str());
-                            }
-                            system_call_names::SystemCallArgumentType::String => {
-                                let mut string = read_string(child, reg as *mut c_void);
-                                let truncated_string = truncate(string.as_str(), 64);
-                                if string.eq(truncated_string) {
-                                    string = format!("{:?}", string);
-                                } else {
-                                    string = format!("{:?}...", truncated_string);
-                                }
-                                output.push_str(string.as_str());
-                            }
-                            system_call_names::SystemCallArgumentType::Address => {
-                                output.push_str(format!("{:?}", value).as_str());
-                            }
-                            system_call_names::SystemCallArgumentType::None => {
-                                continue;
+                    match arg {
+                        system_call_names::SystemCallArgumentType::None => continue,
+                        system_call_names::SystemCallArgumentType::Integer
+                        | system_call_names::SystemCallArgumentType::String
+                        | system_call_names::SystemCallArgumentType::Address => {
+                            if first_comma {
+                                first_comma = false;
+                            } else {
+                                output.push_str(", ")
                             }
                         }
                     }
 
-                    output.push_str(")");
-                    println!("{}", output);
-                } else {
-                    println!(
-                        "[{:?}]: {}() = {:?}",
-                        child.as_raw(),
-                        system_call_names::SYSTEM_CALL_NAMES[(x.orig_rax) as usize],
-                        {
-                            reg = x.rsi;
-                            x
-                        },
-                    )
+                    match arg {
+                        system_call_names::SystemCallArgumentType::Integer => {
+                            output.push_str(format!("{:?}", value).as_str());
+                        }
+                        system_call_names::SystemCallArgumentType::String => {
+                            let mut string = read_string(child, reg as *mut c_void);
+                            let truncated_string = truncate(string.as_str(), 64);
+                            if string.eq(truncated_string) {
+                                string = format!("{:?}", string);
+                            } else {
+                                string = format!("{:?}...", truncated_string);
+                            }
+                            output.push_str(string.as_str());
+                        }
+                        system_call_names::SystemCallArgumentType::Address => {
+                            output.push_str(format!("{:?}", value).as_str());
+                        }
+                        system_call_names::SystemCallArgumentType::None => {
+                            continue;
+                        }
+                    }
                 }
+
+                output.push_str(")");
+                println!("{}", output);
             }
             Err(_) => break,
         };
 
-        println!("{:?}", reg);
-        let stringer = read_string(child, reg as *mut c_void);
-        if stringer != "" {
-            println!("{:?}", stringer);
-        }
         match ptrace::syscall(child, None) {
             Ok(_) => continue,
             Err(_) => break,
