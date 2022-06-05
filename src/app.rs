3d7077d0442769fa1024b07fa82dfe68b87d7433
JakWai01 <jakob.waibel@web.de>
2022-06-05 19:48:23 +0200

    fix: Attaching does no longer require a command

--- src/app.rs
+++ src/app.rs
@@ -3,11 +3,7 @@ use clap::{Arg, Command};
 pub fn build_app() -> Command<'static> {
     let app = Command::new("lurk")
         .author("Jakob Waibel")
-        .arg(
-            Arg::new("command")
-            .help("Trace command"),
-        )
-        .arg(
+        .arg(Arg::new("command").help("Trace command").index(1).required_unless_present("process")).arg(
             Arg::new("syscall-number")
                 .long("syscall-number")
                 .short('n')
