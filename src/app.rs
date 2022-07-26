9e6691eb890d0bea922c95adca91187c4a3f7022
JakWai01 <jakob.waibel@web.de>
2022-07-26 19:54:20 +0200

    fix: Add license to `Cargo.toml`

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,7 @@ use clap::{Arg, Command};
 
 pub fn build_app() -> Command<'static> {
     let app = Command::new("lurk")
-        .version("0.2.1")
+        .version("0.2.2")
         .trailing_var_arg(true)
         .arg(
             Arg::new("syscall-number")
